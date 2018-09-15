03488f80f71b21c5e378b332a43f70e46e35f0c1
Rachelle Luyan Lee <rachelleluyanlee@gmail.com>
2018-09-15 16:57:48 -0400

    something

--- src/App.js
+++ src/App.js
@@ -12,8 +12,38 @@ class App extends Component {
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
+    },
+    inputs: {
+      origin: {
+        lat: 0,
+        lon: 0,
+        speed: 0,
+      },
+      destination: {
+        lat: 0,
+        lon: 0,
+        speed: 0,
+      },
     }
   };
+
+  updateOrigin(lat, lon) {
+    this.setState({
+      origin: {
+        lat,
+        lon,
+      }
+    });
+  } 
+
+  updateDestination(lat, lon) {
+    this.setState({
+      destination: {
+        lat,
+        lon,
+      }
+    });
+  }
 
   render() {
     return (
@@ -25,6 +55,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Origin:
+            <input type="text" value={this.state.origin} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <ReactMapGL
           {...this.state.viewport}
           onViewportChange={(viewport) => this.setState({viewport})}
